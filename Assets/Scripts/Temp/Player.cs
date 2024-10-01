95ee3c89e20c6e54aeae5332bae4a0420fa8cea5
sahanyaL <piyumdi.yunash@gmail.com>
2024-10-01 15:14:15 +0530

    Initial commit

---
+++ Assets/Scripts/Temp/Player.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float moveSpeed;
+    public float rotateAmount;
+    float rot;
+    int score;
+    public GameObject winText;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if(mousePos.x < 0)
+            {
+                rot = rotateAmount;
+            }
+            else
+            {
+                rot = -rotateAmount;
+            }
+
+            transform.Rotate(0, 0, rot);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.up * moveSpeed;
+    }
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Food")
+        {
+            Destroy(collision.gameObject);
+            score++;
+
+            if(score >= 5)
+            {
+                print("Level Complete");
+                winText.SetActive(true);
+            }
+
+        }
+        else if(collision.gameObject.tag == "Danger")
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+}
